43598cd6edf656438b0f7e3c017b9f4e32d37c20
yosepjeon <enekelx1@naver.com>
2021-09-22 23:48:51 +0900

    백준 1991 트리 순회 complete

---
+++ Tree/p1991트리순회/Main.java
@@ -0,0 +1,15 @@
+package Tree.p1991트리순회;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+        int N = scr.nextInt();
+
+        for(int i=0;i<N;i++) {
+
+        }
+    }
+}
