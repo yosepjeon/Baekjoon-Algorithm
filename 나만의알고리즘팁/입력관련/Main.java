107adc8616d35de34195d8de6d60d2ab5e8f190a
yosepjeon <enekelx1@naver.com>
2021-09-29 22:23:19 +0900

    나만의 알고리즘 팁

---
+++ 나만의알고리즘팁/입력관련/Main.java
@@ -0,0 +1,63 @@
+package 나만의알고리즘팁.입력관련;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    // 입력을 받는 함수와
+    static void input() {
+        FastReader scr = new FastReader();
+    }
+
+    // 문제를 푸는 함수를 분리
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
