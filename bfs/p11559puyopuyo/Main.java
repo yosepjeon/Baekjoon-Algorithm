da805d349e725e72de9887917af16b2b3339ed98
yosepjeon <enekelx1@naver.com>
2022-01-10 23:56:46 +0900

    p11559 complete

--- bfs/p11559puyopuyo/Main.java
+++ bfs/p11559puyopuyo/Main.java
@@ -1,7 +1,10 @@
 package bfs.p11559puyopuyo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
 
     }
 }
